539abab14e73a0ed08886fe190e5f7e373b61c4d
toThePyPoint <tothepypoint@gmail.com>
2026-05-26 15:14:41 +0200

    feat(vttp): resolve transport number from delivery

--- ZPP3U_RETRIEVING_DATA.py
+++ ZPP3U_RETRIEVING_DATA.py
@@ -20,8 +20,11 @@ from sap_rtab import rfc_read_table
 
 if __name__ == "__main__":
 
-    variant_name = "REP_LU_KPI_ALL"
+    variant_name = "REP_BM_KPI_ALL"
     # variant_name = sys.argv[1]
+
+    SAP_SYSTEM = "P11_SSO"
+    # SAP_SYSTEM = "K11"
 
     BASE_PATH = Path(r"P:\Technisch\PLANY PRODUKCJI\PLANIŚCI\PP_TOOLS_TEMP_FILES\03_ZPP3U_RETRIEVING_DATA")
     ERROR_LOG_PATH = BASE_PATH / "error.log"
@@ -88,8 +91,10 @@ if __name__ == "__main__":
         df['products_group'] = ""
         df['quantity_of_positions'] = 1
 
+        df['customer_order'] = df['customer_order'].str.strip().str.zfill(10)
+
         # TODO: Implement the logic for retriving author and transport number with PyRFC
-        customer_orders_list = df['customer_order'].str.strip().str.zfill(10).to_list()
+        customer_orders_list = df['customer_order'].to_list()
 
         customer_ord_filter_vbeln = " OR ".join(
             [f"VBELN = '{num}'" for num in customer_orders_list]
@@ -103,14 +108,14 @@ if __name__ == "__main__":
 
         print(customer_ord_filter_vbeln)
 
-        with get_conn("K11") as conn:
+        with get_conn(SAP_SYSTEM) as conn:
 
             vbak = rfc_read_table(
                 conn=conn,
                 table="VBAK",
                 fields=[
-                    "VBELN",
-                    "ERNAM",
+                    "VBELN", # customer order number
+                    "ERNAM", # customer order author
                 ],
                 where=f"""
                     {customer_ord_filter_vbeln}
@@ -141,24 +146,62 @@ if __name__ == "__main__":
                 vbfa_df["VBTYP_N"] == "J"
                 ]
 
-        df.to_excel(fr"{BASE_PATH}\main_df.xlsx", index=False)
         vbak_df.to_excel(fr"{BASE_PATH}\vbak_df.xlsx", index=False)
         delivery_df.to_excel(fr"{BASE_PATH}\delivery_df.xlsx", index=False)
 
+        df = df.merge(delivery_df, left_on=['customer_order', 'customer_order_position'], right_on=['VBELV', 'POSNV'] , how='left')
+        df.rename(columns={'VBELN': 'delivery_number', 'POSNN': 'delivery_position'}, inplace=True)
+
+        df = df.merge(vbak_df, left_on=['customer_order'], right_on=['VBELN'], how='left')
+        df.rename(columns={'ERNAM': 'creator'}, inplace=True)
+        df.drop(columns=["VBELV", "VBELN", "POSNV"], inplace=True)
+
         print(vbak_df)
         print(delivery_df)
+        print(df)
+        df.to_excel(fr"{BASE_PATH}\main_df.xlsx", index=False)
+
+        deliveries_list = delivery_df['VBELN'].str.strip().str.zfill(10).to_list()
+
+        if deliveries_list:
+            delivery_num_filter_vbeln = " OR ".join(
+                [f"VBELN = '{num}'" for num in deliveries_list]
+            )
+
+            with get_conn(SAP_SYSTEM) as conn:
+
+                vttp = rfc_read_table(
+                    conn=conn,
+                    table="VTTP",
+                    fields=[
+                        "VBELN",
+                        "TKNUM",
+                    ],
+                    where=f"""
+                        {delivery_num_filter_vbeln}
+                    """
+                )
+
+            vttp_df = pd.DataFrame(vttp, columns=["VBELN", "TKNUM"])
+        else:
+            vttp_df = pd.DataFrame(columns=["VBELN", "TKNUM"])
+        vttp_df.to_excel(fr"{BASE_PATH}\vttp_df.xlsx", index=False)
+
+        df = df.merge(vttp_df, left_on=['delivery_number'], right_on=['VBELN'], how='left')
+        df.rename(columns={'TKNUM': 'transport_number'}, inplace=True)
+        df['transport_number'] = df['transport_number'].fillna("transport number not found")
 
         df["doc_date"] = pd.to_datetime(df["doc_date"], format="%d.%m.%Y").dt.strftime("%Y-%m-%d")
-        df = df[["doc_date", "customer_order", "customer_order_position", "quantity_of_positions", "Description(PL)", "Description(EN)", "products_group", "creator", "route"]]
+        df = df[["doc_date", "customer_order", "customer_order_position", "quantity_of_positions", "Description(PL)", "Description(EN)", "products_group", "creator", "transport_number"]]
 
-        df_gr = df.groupby(['customer_order', 'route'])['quantity_of_positions'].sum().reset_index()
+        df_gr = df.groupby(['customer_order', 'transport_number'])['quantity_of_positions'].sum().reset_index()
 
-        df = df.drop_duplicates(subset=['customer_order', 'route'], keep='last')
-        df = df.merge(df_gr, on=['customer_order', 'route'], how='left')
+        df = df.drop_duplicates(subset=['customer_order', 'transport_number'], keep='last')
+        df = df.merge(df_gr, on=['customer_order', 'transport_number'], how='left')
         df = df.drop(columns=['quantity_of_positions_x'])
         df.rename(columns={'quantity_of_positions_y': 'quantity_of_positions_sum'}, inplace=True)
         df = df[['doc_date', 'customer_order', 'quantity_of_positions_sum', 'Description(PL)', 'Description(EN)',
-                 'products_group', 'creator', 'route']]
+                 'products_group', 'creator', 'transport_number']]
 
         # Convert DataFrame to clipboard-friendly format
         clipboard_data = df.to_csv(sep='\t', index=False, header=False)
